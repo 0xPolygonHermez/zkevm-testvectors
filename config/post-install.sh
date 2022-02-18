3332da5808b7be86fdfa6821b83d19d206116847
Ignasirv <ignasirv@hotmail.com>
2022-02-18 10:13:12 +0100

    Support postinstall script for macOS

--- config/post-install.sh
+++ config/post-install.sh
@@ -2,10 +2,13 @@
 # - 'putRaw' method on 'SecureTrie' to store [key, values] without hashing the key
 # - 'putContractStorageRaw' on 'StateManager' to allow loading storage without hashing the key twice
 
-SCRIPT=$(readlink -f "$0")
-BUILD_CONFIG_HOME=$(dirname "$SCRIPT")
+# No need to set readlink of macOS users
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    SCRIPT=$(readlink -f "$0")
+    BUILD_CONFIG_HOME=$(dirname "$SCRIPT")
 
-cd ${BUILD_CONFIG_HOME}/..
+    cd ${BUILD_CONFIG_HOME}/..
+fi
 
 cp config/secure.js node_modules/merkle-patricia-tree/dist/secure.js
 cp config/stateManager.js node_modules/@ethereumjs/vm/dist/state/stateManager.js
