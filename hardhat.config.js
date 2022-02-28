e1973d696a0faef46fad0b867e93fa1e3e2e7374
invocamanman <jesus.ligero@yahoo.es>
2022-02-28 17:54:12 +0100

    add sc test vectors

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+DEFAULT_MNEMONIC = "test test test test test test test test test test test junk";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          }
+        }
+      },
+      {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: DEFAULT_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
+  }
+};
+
