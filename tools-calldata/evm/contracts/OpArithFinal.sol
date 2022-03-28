ad4cb47c532378f6b00a436af621b44fe9a00725
krlosMata <krlosmata@gmail.com>
2022-03-28 13:22:25 +0200

    add test opcodes SUB, MUL, DIV

--- tools-calldata/evm/contracts/OpArithFinal.sol
+++ tools-calldata/evm/contracts/OpArithFinal.sol
@@ -5,11 +5,42 @@ contract OpArithFinal {
 
     uint256 res = 1;
 
+    // opcode 0x00
+    function opStop() public pure {
+        assembly {
+            stop()
+        }
+    }
+
     // opcode 0x01
     function opAdd() public {
         assembly {
-            let tmpAdd := add(1, 2)
-            sstore(0x0, tmpAdd)
+            let tmp := add(1, 2)
+            sstore(0x0, tmp)
+        }
+    }
+
+    // opcode 0x02
+    function opMul() public {
+        assembly {
+            let tmp := mul(3, 4)
+            sstore(0x0, tmp)
+        }
+    }
+
+    // opcode 0x03
+    function opSub() public {
+        assembly {
+            let tmp := sub(6, 5)
+            sstore(0x0, tmp)
+        }
+    }
+
+    // opcode 0x04
+    function opDiv() public {
+        assembly {
+            let tmp := div(10, 2)
+            sstore(0x0, tmp)
         }
     }
 }
