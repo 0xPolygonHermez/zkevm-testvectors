e512306c335287d4e88fc3b44b9e39bc7b3311c6
laisolizq <laisolizq@gmail.com>
2022-04-07 18:47:00 +0200

    Add inputs mstore & mload

---
+++ tools-calldata/evm/contracts/OpMstoreMload.sol
@@ -0,0 +1,210 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract OpMstoreMload {
+
+    uint256 a = 0x11111111111111111111111111111111111111111111111111111111111111;
+
+    function opSimple() public {
+        assembly {
+            let b := sload(0x0)
+            mstore(0xa0, b)
+            let c := mload(0xa0)
+            sstore(1, c)
+        }
+    }
+
+    function opComplex() public {
+        assembly {
+            let b := sload(0x0)
+            mstore(0xa5, b)
+            let c := mload(0xa5)
+            sstore(1, c)
+        }
+    }
+
+    function opCodeCopySimple() public {
+        assembly {
+            codecopy(0xa0, 10, 32)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa0)
+            sstore(0x0, result)
+        }
+    }
+
+    function opCodeCopyComplex() public {
+        assembly {
+            codecopy(0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa5)
+            sstore(0x0, result)
+        }
+    }
+
+    function opCodeCopyComplex2() public {
+        assembly {
+            codecopy(0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xbe, b)
+            let result := mload(0xbe)
+            sstore(0x0, result)
+        }
+    }
+
+    function opCodeCopyComplex3() public {
+        assembly {
+            codecopy(0xa0, 10, 68)
+            let b := mload(0xb0)
+            mstore(0xb9, b)
+            let result := mload(0xb9)
+            sstore(0x0, result)
+            result := mload(0xe0)
+            sstore(0x1, result)
+        }
+    }
+
+    function opExtCodeCopySimple(address addr) public {
+        assembly {
+            extcodecopy(addr, 0xa0, 10, 32)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa5)
+            sstore(0x0, result)
+        }
+    }
+
+    function opExtCodeCopyComplex(address addr) public {
+        assembly {
+            extcodecopy(addr, 0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa5)
+            sstore(0x0, result)
+        }
+    }
+
+    function opExtCodeCopyComplex2(address addr) public {
+        assembly {
+            extcodecopy(addr, 0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xbe, b)
+            let result := mload(0xbe)
+            sstore(0x0, result)
+        }
+    }
+
+    function opExtCodeCopyComplex3(address addr) public {
+        assembly {
+            extcodecopy(addr, 0xa0, 10, 68)
+            let b := mload(0xb0)
+            mstore(0xb9, b)
+            let result := mload(0xb9)
+            sstore(0x0, result)
+            result := mload(0xe0)
+            sstore(0x1, result)
+        }
+    }
+
+    function auxReturn() external returns(uint256){
+        return 0x123456789;
+    }
+    function opReturnDataCopySimple() external returns (uint256) {
+        uint256 aux = this.auxReturn();
+        require(aux != 0);
+        assembly {
+            returndatacopy(0xa0, 0, 29)
+            let result := mload(0xa0)
+            sstore(0x0, result)
+        }
+        return aux;
+    }
+
+    function opReturnDataCopyComplex() external returns (uint256) {
+        uint256 aux = this.auxReturn();
+        require(aux != 0);
+        assembly {
+            returndatacopy(0xa3, 0, 32)
+            let result := mload(0xa3)
+            sstore(0x0, result)
+            result := mload(0xa0)
+            sstore(0x1, result)
+            result := mload(0xc0)
+            sstore(0x2, result)
+        }
+        return aux;
+    }
+
+    function opCallDataCopySimple() public {
+        assembly {
+            calldatacopy(0xa0, 0, 32)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa5)
+            sstore(0x0, result)
+        }
+    }
+
+    function opCallDataCopyComplex() public {
+        assembly {
+            calldatacopy(0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xa5, b)
+            let result := mload(0xa0)
+            sstore(0x0, result)
+            result := mload(0xa5)
+            sstore(0x1, result)
+        }
+    }
+
+    function opCallDataCopyComplex2() public {
+        assembly {
+            calldatacopy(0xa0, 10, 22)
+            let b := mload(0xa0)
+            mstore(0xbe, b)
+            let result := mload(0xa0)
+            sstore(0x0, result)
+            result := mload(0xbe)
+            sstore(0x1, result)
+        }
+    }
+
+    function opCallDataCopyComplex3() public {
+        assembly {
+            calldatacopy(0xa0, 10, 68)
+            let b := mload(0xb0)
+            mstore(0xb9, b)
+            let result := mload(0xb9)
+            sstore(0x0, result)
+            result := mload(0xe0)
+            sstore(0x1, result)
+        }
+    }
+
+    function opCallDataCopyComplex4() public {
+        assembly {
+            calldatacopy(0xa0, 30, 70)
+            let b := mload(0xb0)
+            mstore(0xb9, b)
+            let result := mload(0xb9)
+            sstore(0x0, result)
+            result := mload(0xe0)
+            sstore(0x1, result)
+        }
+    }
+
+    function auxReturn2() external returns (uint40[5] memory){
+        return [0x123456789, 0x123456789, 0x123456789, 0x123456789, 0x123456789];
+    }
+
+    function opReturnDataCopyComplex2() external returns (uint40[5] memory) {
+        uint40[5] memory aux = this.auxReturn2();
+        assembly {
+            returndatacopy(0xa0, 0, 64)
+            let result := mload(0xbd)
+            sstore(0x0, result)
+        }
+        return aux;
+    }
+}
