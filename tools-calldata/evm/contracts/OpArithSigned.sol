78bdf7317c580c8090ffa352800465bf4eeaa494
invocamanman <jesus.ligero@yahoo.es>
2022-04-16 04:53:30 +0200

    slt sgt

--- tools-calldata/evm/contracts/OpArithSigned.sol
+++ tools-calldata/evm/contracts/OpArithSigned.sol
@@ -61,4 +61,78 @@ contract OpArithSigned {
   }
 
   // opcode 0x0b
+
+  // opcode 0x12
+  function opSLT1() public {
+    int256 auxNegative = -10;
+    int256 auxNegative2 = 3;
+    assembly {
+      let result := slt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSLT2() public {
+    int256 auxNegative = 10;
+    int256 auxNegative2 = -3;
+    assembly {
+      let result := slt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSLT3() public {
+    int256 auxNegative = -10;
+    int256 auxNegative2 = -3;
+    assembly {
+      let result := slt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSLT4() public {
+    int256 auxNegative = 10;
+    int256 auxNegative2 = 3;
+    assembly {
+      let result := slt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  // opcode 13
+  function opSGT1() public {
+    int256 auxNegative = -10;
+    int256 auxNegative2 = 3;
+    assembly {
+      let result := sgt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSGT2() public {
+    int256 auxNegative = 10;
+    int256 auxNegative2 = -3;
+    assembly {
+      let result := sgt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSGT3() public {
+    int256 auxNegative = -10;
+    int256 auxNegative2 = -3;
+    assembly {
+      let result := sgt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
+
+  function opSGT4() public {
+    int256 auxNegative = 10;
+    int256 auxNegative2 = 3;
+    assembly {
+      let result := sgt(auxNegative, auxNegative2)
+      sstore(0x0, result)
+    }
+  }
 }
