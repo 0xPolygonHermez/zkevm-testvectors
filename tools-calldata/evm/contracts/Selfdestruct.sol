3d334e2fc2123df5bd534f42d891f9b338bcaa53
Ignasirv <ignasirv@hotmail.com>
2022-04-18 22:30:19 +0200

    Test for selfdestruct

---
+++ tools-calldata/evm/contracts/Selfdestruct.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Selfdestruct {
+
+    address receiver;
+    uint256 public value;
+
+    function setReceiver(address _receiver) public {
+        receiver = _receiver;
+    }
+
+    function setVal() public payable {
+        value = msg.value;
+    }
+
+    function destruct() public {
+        selfdestruct(payable(receiver));
+    }
+
+    function destructWarm() public {
+        address tmp = receiver;
+        assembly {
+            let result := balance(tmp)
+        }
+        selfdestruct(payable(receiver));
+    }
+}
