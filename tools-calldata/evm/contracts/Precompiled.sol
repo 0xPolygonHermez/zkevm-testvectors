ac195c2792a42e0291ab3e961e81e274a3637925
laisolizq <laisolizq@gmail.com>
2022-04-28 18:49:17 +0200

    Add precompiled ecAdd

--- tools-calldata/evm/contracts/Precompiled.sol
+++ tools-calldata/evm/contracts/Precompiled.sol
@@ -84,4 +84,24 @@ contract Precompiled {
             }
         }
     }
+
+    function ecAdd_0(bytes32 ax, bytes32 ay, bytes32 bx, bytes32 by) public returns (bytes32[2] memory result) {
+        bytes32[4] memory input;
+        input[0] = ax;
+        input[1] = ay;
+        input[2] = bx;
+        input[3] = by;
+        assembly {
+            let success := call(gas(), 0x06, 0, input, 0x80, result, 0x40)
+            switch success
+            case 0 {
+                revert(0,0)
+            }
+            let result1 := mload(0x80)
+            let result2 := mload(0xa0)
+            sstore(0x0, result1)
+            sstore(0x1, result2)
+        }
+        return result;
+    }
 }
