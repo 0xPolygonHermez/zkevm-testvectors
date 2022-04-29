6f9706914fed292dd4dd8cd5e47047d31c452406
laisolizq <laisolizq@gmail.com>
2022-04-29 11:16:05 +0200

    Add precompiled ecPairing

--- tools-calldata/evm/contracts/Precompiled.sol
+++ tools-calldata/evm/contracts/Precompiled.sol
@@ -123,4 +123,21 @@ contract Precompiled {
         }
         return result;
     }
+
+    function ecPairing_0(bytes memory input) public returns (bytes32 result) {
+        // input is a serialized bytes stream of (a1, b1, a2, b2, ..., ak, bk) from (G_1 x G_2)^k
+        uint256 len = input.length;
+        require(len % 192 == 0);
+        assembly {
+            let memPtr := mload(0x40)
+            let success := call(gas(), 0x08, 0, add(input, 0x20), len, memPtr, 0x20)
+            switch success
+            case 0 {
+                revert(0,0)
+            } default {
+                result := mload(memPtr)
+            }
+            sstore(0x0, success)
+        }
+    }
 }
