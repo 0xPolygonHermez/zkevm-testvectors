b2e80fc60d9ec7117a239276c6ce2e23ce4c8ae7
laisolizq <laisolizq@gmail.com>
2022-05-05 16:11:01 +0200

    Add more test & delete system opcodes template

--- tools-calldata/evm/contracts/PreEcrecover.sol
+++ tools-calldata/evm/contracts/PreEcrecover.sol
@@ -44,12 +44,23 @@ contract PreEcrecover {
         retEcrecover = ecrecover(messHash, v, r, s);
     }
 
-    // r out of range: r > 115792089237316195423570985008687907852837564279074904382605163141518161494337
+    // s out of range (valid): s > 57896044618658097711785492504343953926418782139537452191302581570759080747169
     function preEcrecover_4() public {
         bytes32 messHash = 0x456e9aea5e197a1f1af7a3e85a3212fa4049a3ba34c2289b4c860fc0b0c64ef3;
         uint8 v = 27;
         bytes32 r = 0x9242685bf161793cc25603c231bc2f568eb630ea16aa137d2664ac8038825608;
         bytes32 s = 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20af;
+
+        retEcrecover = ecrecover(messHash, v, r, s);
+    }
+
+
+    // s out of range (no valid): s > 115792089237316195423570985008687907852837564279074904382605163141518161494337
+    function preEcrecover_5() public {
+        bytes32 messHash = 0x456e9aea5e197a1f1af7a3e85a3212fa4049a3ba34c2289b4c860fc0b0c64ef3;
+        uint8 v = 27;
+        bytes32 r = 0x9242685bf161793cc25603c231bc2f568eb630ea16aa137d2664ac8038825608;
+        bytes32 s = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364144;
 
         retEcrecover = ecrecover(messHash, v, r, s);
     }
