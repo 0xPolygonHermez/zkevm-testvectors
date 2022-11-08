06856920c4f2b01c00c3379d4577fd5dcaec14be
laisolizq <laisolizq@gmail.com>
2022-11-08 12:59:52 +0100

    add tests modexp

--- tools-calldata/evm/contracts/PreModExp.sol
+++ tools-calldata/evm/contracts/PreModExp.sol
@@ -45,9 +45,117 @@ contract PreModExp {
             mstore(add(memPtr, 0x40), 0x20)
 
             // assign base, exponent, modulus
+            mstore(add(memPtr, 0x60), 0xffff)
+            mstore(add(memPtr, 0x80), 0xffff)
+            mstore(add(memPtr, 0xa0), 0xea5)
+
+            // call the precompiled contract BigModExp (0x05)
+            let success := call(gas(), 0x05, 0x0, memPtr, 0xc0, memPtr, 0x20)
+            switch success
+            case 0 {
+                revert(0x0, 0x0)
+            } default {
+                result := mload(memPtr)
+                sstore(0x1,result)
+            }
+        }
+    }
+    function modexp_2() public {
+        bytes memory result;
+        assembly {
+            // free memory pointer
+            let memPtr := mload(0x40)
+
+            // length of base, exponent, modulus
+            mstore(memPtr, 0x20)
+            mstore(add(memPtr, 0x20), 0x20)
+            mstore(add(memPtr, 0x40), 0x20)
+
+            // assign base, exponent, modulus
             mstore(add(memPtr, 0x60), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
-            mstore(add(memPtr, 0x80), 0xea0)
-            mstore(add(memPtr, 0xa0), 0xea0)
+            mstore(add(memPtr, 0x80), 0xffff)
+            mstore(add(memPtr, 0xa0), 0xea6)
+
+            // call the precompiled contract BigModExp (0x05)
+            let success := call(gas(), 0x05, 0x0, memPtr, 0xc0, memPtr, 0x20)
+            switch success
+            case 0 {
+                revert(0x0, 0x0)
+            } default {
+                result := mload(memPtr)
+                sstore(0x1,result)
+            }
+        }
+    }
+    function modexp_3() public {
+        bytes memory result;
+        assembly {
+            // free memory pointer
+            let memPtr := mload(0x40)
+
+            // length of base, exponent, modulus
+            mstore(memPtr, 0x20)
+            mstore(add(memPtr, 0x20), 0x20)
+            mstore(add(memPtr, 0x40), 0x20)
+
+            // assign base, exponent, modulus
+            mstore(add(memPtr, 0x60), 0xea7)
+            mstore(add(memPtr, 0x80), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            mstore(add(memPtr, 0xa0), 0xffff)
+
+            // call the precompiled contract BigModExp (0x05)
+            let success := call(gas(), 0x05, 0x0, memPtr, 0xc0, memPtr, 0x20)
+            switch success
+            case 0 {
+                revert(0x0, 0x0)
+            } default {
+                result := mload(memPtr)
+                sstore(0x1,result)
+            }
+        }
+    }
+    function modexp_4() public {
+        bytes memory result;
+        assembly {
+            // free memory pointer
+            let memPtr := mload(0x40)
+
+            // length of base, exponent, modulus
+            mstore(memPtr, 0x20)
+            mstore(add(memPtr, 0x20), 0x20)
+            mstore(add(memPtr, 0x40), 0x20)
+
+            // assign base, exponent, modulus
+            mstore(add(memPtr, 0x60), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            mstore(add(memPtr, 0x80), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            mstore(add(memPtr, 0xa0), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+
+            // call the precompiled contract BigModExp (0x05)
+            let success := call(gas(), 0x05, 0x0, memPtr, 0xc0, memPtr, 0x20)
+            switch success
+            case 0 {
+                revert(0x0, 0x0)
+            } default {
+                result := mload(memPtr)
+                sstore(0x1,result)
+            }
+        }
+    }
+    function modexp_5() public {
+        bytes memory result;
+        assembly {
+            // free memory pointer
+            let memPtr := mload(0x40)
+
+            // length of base, exponent, modulus
+            mstore(memPtr, 0x20)
+            mstore(add(memPtr, 0x20), 0x20)
+            mstore(add(memPtr, 0x40), 0x20)
+
+            // assign base, exponent, modulus
+            mstore(add(memPtr, 0x60), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            mstore(add(memPtr, 0x80), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            mstore(add(memPtr, 0xa0), 0xea9)
 
             // call the precompiled contract BigModExp (0x05)
             let success := call(gas(), 0x05, 0x0, memPtr, 0xc0, memPtr, 0x20)
