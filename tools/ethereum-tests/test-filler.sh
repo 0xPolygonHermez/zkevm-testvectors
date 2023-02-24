d1c2795de78bcf3bf656b478bb844ca90d518c92
laisolizq <laisolizq@gmail.com>
2023-02-24 11:01:52 +0100

    fixes

--- tools/ethereum-tests/test-filler.sh
+++ tools/ethereum-tests/test-filler.sh
@@ -3,13 +3,13 @@
 #     2 - Download dretesteth.tar docker image from http://retesteth.ethdevops.io/dretesteth.tar
 #     3 - Load docker container from image with `docker load -i dretest*.tar`
 #     4 - Update filler files to regenerate
-#     5 -  Run setup.sh
+#     5 - Run setup.sh
 #     6 - Run this script setting the vars
 #
 #     sh test-filler.sh [-f/--folder fill all tests from folder]
 
 test_folder_name=$1
-absolute_tests_path=/Users/ignasi/Documents/Github/Polygon/zkevm-testvectors/tools/ethereum-tests/tests
+absolute_tests_path=$(pwd)/tests
 test_file_name=$2
 
 # Read args
@@ -18,9 +18,11 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     -f|--folder)
       run_folder=true
+      test_folder_name=$2
       shift # past argument
       ;;
   esac
+  shift
 done
 # Download filler script
 if [ -f "dretesteth.sh" ]; then
@@ -35,10 +37,10 @@ if [ "$run_folder" = true ]; then
     # Run dretesteth from docker
     ./dretesteth.sh -t GeneralStateTests/$test_folder_name -- --testpath $absolute_tests_path --fillchain
     # Regen test as executor input
-    npx mocha gen-inputs.js --evm-debug --folder $test_folder_name
+    # npx mocha gen-inputs.js --evm-debug --folder $test_folder_name
 else
     # Run dretesteth from docker
     ./dretesteth.sh -t GeneralStateTests/$test_folder_name -- --testpath $absolute_tests_path --singletest $test_file_name --fillchain
     # Regen test as executor input
-    npx mocha gen-inputs.js --evm-debug --test $test_folder_name/$test_file_name.json
+    # npx mocha gen-inputs.js --evm-debug --test $test_folder_name/$test_file_name.json
 fi
