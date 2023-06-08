8dde6e169c15afc8d5af546925e7a1b530eb1327
krlosMata <krlosmata@gmail.com>
2023-06-08 11:28:10 +0200

    add test tricky depth

---
+++ tools-calldata/evm/contracts/Depth.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+import "./IOpCallAux.sol";
+
+contract Depth {
+
+    IOpCallAux openv;
+    uint test = 0;
+    bytes32 constant auxReturn = 0x6aecbc3300000000000000000000000000000000000000000000000000000000;
+
+    function start(address addr, uint256 gasForwarded) public {
+        test = this.secondCall{gas: gasForwarded}(addr);
+    }
+
+    function secondCall(address addr) external returns (uint256) {
+        uint256 success;
+        assembly {
+            mstore(0x80, auxReturn)
+            success := staticcall(gas(), addr, 0x80, 0x04, 0x80, 0x20)
+        }
+    }
+}
