fa74658ea8647e12e0986913dc08e5c520c1dc4b
krlosMata <krlosmata@gmail.com>
2023-08-11 09:07:40 +0200

    update test with effective percentage

--- tools/modify-inputs.js
+++ tools/modify-inputs.js
@@ -1,36 +1,55 @@
-const fs = require("fs");
-const path = require("path");
-const dir = "../executor/";
+const fs = require('fs');
+const path = require('path');
+const { contractUtils } = require('@0xpolygonhermez/zkevm-commonjs');
+const { argv } = require('yargs')
+    .alias('d', 'dir');
 
 async function main() {
+    // path dir
+    const dir = typeof argv.dir === 'string' ? argv.dir.trim() : undefined;
 
-    const folders = []
-    folders.push(path.join(__dirname, dir.trim()))
+    if (dir === undefined) {
+        throw new Error('"Dir" parameter is undefined');
+    }
 
+    const folders = [];
+    folders.push(path.join(__dirname, dir.trim()));
+    console.log(folders);
     while (folders.length > 0) {
-        const files = folders.pop()
-        fs.readdirSync(files).forEach(file => {
-            const fileDir = `${files}${file}`
-            //Print file name
-            console.log(file)
-            //Check is a dir
-            if(fs.lstatSync(fileDir).isDirectory()) {
-                folders.push(`${fileDir}/`)
-                return
+        const files = folders.pop();
+        fs.readdirSync(files).forEach((file) => {
+            const fileDir = `${files}/${file}`;
+            // Check is a dir
+            if (fs.lstatSync(fileDir).isDirectory()) {
+                folders.push(`${fileDir}/`);
+                return;
             }
-
-            //Check is json files
-            if(!file.endsWith(".json")) {
-                return
+            // Check is json files
+            if (!file.endsWith('.json')) {
+                return;
             }
-            const data = JSON.parse(fs.readFileSync(fileDir, "utf8"));
-            // TODO: Update the json content
-            delete data["keys"]
+            // Print file name
+            const data = JSON.parse(fs.readFileSync(fileDir, 'utf8'));
 
-            //Update data
-            fs.writeFileSync(fileDir, JSON.stringify(data, null, 2));
-            console.log("Updated")
-        })
+            if (data.batchL2Data.slice(-2) !== 'ff') {
+                data.batchL2Data += 'ff';
+                data.batchHashData = contractUtils.calculateBatchHashData(
+                    data.batchL2Data,
+                );
+
+                data.newAccInputHash = contractUtils.calculateAccInputHash(
+                    data.oldAccInputHash,
+                    data.batchHashData,
+                    data.globalExitRoot,
+                    data.timestamp,
+                    data.sequencerAddr,
+                );
+
+                // Update data
+                fs.writeFileSync(fileDir, JSON.stringify(data, null, 2));
+                console.log(`Update ${fileDir}`);
+            }
+        });
     }
 }
 
