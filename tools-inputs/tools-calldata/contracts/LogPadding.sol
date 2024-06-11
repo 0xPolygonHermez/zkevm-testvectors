6ed03ffa5fec340a2cb5520f89456d092c43d5b2
krlosMata <krlosmata@gmail.com>
2024-06-11 16:44:54 +0200

    more logs tests

--- tools-inputs/tools-calldata/contracts/LogPadding.sol
+++ tools-inputs/tools-calldata/contracts/LogPadding.sol
@@ -13,4 +13,14 @@ contract LogPadding {
             log1(dataPtr, dataLength, eventSignature)
         }
     }
+
+    function emitEventLarger(uint256 lengthLog) public {
+        bytes memory myData = hex"0102030405060708090A0B0C0D0E0F0102030405060708090A0B0C0D0E0F0102030405060708090A0B0C0D0E0F0102030405060708090A0B0C0D0E0F";
+        bytes32 eventSignature = keccak256("Test1(bytes)");
+        assembly {
+            let dataPtr := add(myData, 32)
+            let dataLength := mload(myData)
+            log1(dataPtr, lengthLog, eventSignature)
+        }
+    }
 }
